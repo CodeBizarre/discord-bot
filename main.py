4edf6d55d088c9a21b63c262a6034cffe35719f3
Aurexine <Aurexine@gmail.com>
2020-08-08 03:56:55 -0500

    Comment cleanup

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ def main():
     async def on_ready():
         # If this is the first launch (Not a reconnection from disconnect)
         if bot.first_launch:
-            # Load core modules
+            # Load core modules first
             bot.load_extension("core")
             bot.load_extension("accounts")
             bot.load_extension("plugin_manager")
-            # Load all available plugins
+
+            # Followed by available plugins
             for p in sorted(os.listdir("plugins")):
                 if not p.endswith(".py"):
                     continue
@@ -33,15 +34,13 @@ def main():
                     exc = "{0}: {1}".format(type(e).__name__, e)
                     bot.log.warning(f"Failed to load plugin {p}:\n    - {exc}")
 
-            # Set the DiscordBot instance's application info
             bot.app_info = await bot.application_info()
 
             bot.first_launch = False
 
-        # Print mission control to the console
         bot.log.info("\n".join(bot.mission_control()))
 
-        # Register the servers that the bot has joined
+        # Ensure all currently joined severs are registered
         for server in bot.guilds:
             sid = str(server.id)
             if sid not in bot.servers:
