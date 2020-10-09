3894ebef28994a38011eeeb3dbb06a903ed20bbd
Aurexine <Aurexine@gmail.com>
2020-10-09 07:38:19 -0500

    Long overdue version bump

--- src/discordbot/core/discord_bot.py
+++ src/discordbot/core/discord_bot.py
@@ -10,7 +10,7 @@ from discord.ext import commands
 
 from discordbot.core.time_tools import pretty_datetime
 
-VERSION = "3.1.0b1"
+VERSION = "3.2.0b1"
 
 def get_logger(file_name) -> logging.Logger:
     """Get an instance of Logger and set up log files."""
