a9a457ac3fd04bca4de1ce2df99b2076f8477e78
Aurexine <Aurexine@gmail.com>
2020-11-12 02:01:29 -0600

    Correct type annotation

--- src/discordbot/core/db_tools.py
+++ src/discordbot/core/db_tools.py
@@ -1,6 +1,7 @@
+from typing import Union, List
 from sqlitedict import SqliteDict
 
-def update_db(sql_db: SqliteDict, dict_db: dict, base_key: str):
+def update_db(sql_db: SqliteDict, dict_db: Union[dict, List[str]], base_key: str):
     """Update the SQLite DB[key] with the in-memory json copy after changes."""
     try:
         sql_db[base_key] = dict_db
